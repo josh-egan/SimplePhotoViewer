dd0f5d8f619fca809c3479feffe3be6cdcfb6eed
Joshua Egan <josh.egan145@gmail.com>
2014-08-20 17:56:56 -0600

    Removed the code that will be written during the kata. The project builds and will run. Uncomment the tests and then write the code.

--- src/SimplePhotoViewer/UI/ViewModels/ImageViewModel.cs
+++ src/SimplePhotoViewer/UI/ViewModels/ImageViewModel.cs
@@ -9,128 +9,19 @@ namespace SimplePhotoViewer.UI.ViewModels
 {
     public interface IImageViewModel : INotifyPropertyChangedEx
     {
-        void ReSelectFile();
         event EventHandler<bool> IsImageSelectedChanged;
+        void ReSelectFile();
     }
 
     public class ImageViewModel : PropertyChangedBase, IImageViewModel
     {
-        private readonly IFileTraverser fileTraverser;
 
-        private BitmapSource currentImage;
-        private Visibility currentImageVisibility;
-        private Visibility nextVisibility;
-        private Visibility previousVisibility;
-        private Visibility selectFileVisibility;
-
-        [Inject]
-        public ImageViewModel(IFileTraverser fileTraverser)
-        {
-            this.fileTraverser = fileTraverser;
-
-            UpdateVisibilities();
-        }
-
-        public BitmapSource CurrentImage
-        {
-            get { return currentImage; }
-            set
-            {
-                currentImage = value;
-                NotifyOfPropertyChange(() => CurrentImage);
-            }
-        }
-
-        public Visibility CurrentImageVisibility
-        {
-            get { return currentImageVisibility; }
-            set
-            {
-                currentImageVisibility = value;
-                NotifyOfPropertyChange(() => CurrentImageVisibility);
-            }
-        }
-
-        public Visibility PreviousVisibility
-        {
-            get { return previousVisibility; }
-            set
-            {
-                previousVisibility = value;
-                NotifyOfPropertyChange(() => PreviousVisibility);
-            }
-        }
-
-        public Visibility NextVisibility
-        {
-            get { return nextVisibility; }
-            set
-            {
-                nextVisibility = value;
-                NotifyOfPropertyChange(() => NextVisibility);
-            }
-        }
-
-        public Visibility SelectFileVisibility
-        {
-            get { return selectFileVisibility; }
-            set
-            {
-                selectFileVisibility = value;
-                NotifyOfPropertyChange(() => SelectFileVisibility);
-            }
-        }
-
-        private bool IsImageSelected
-        {
-            get { return CurrentImage != null; }
-        }
-
-        public event EventHandler<bool> IsImageSelectedChanged = delegate { };
+        public event EventHandler<bool> IsImageSelectedChanged;
 
         public void ReSelectFile()
         {
-            HandleNewFilePath(fileTraverser.SelectFile(Constants.SupportedImageExtensions));
+            throw new NotImplementedException();
         }
 
-        public void SelectFile()
-        {
-            HandleNewFilePath(fileTraverser.SelectFile(Constants.SupportedImageExtensions));
-        }
-
-        public void Next()
-        {
-            HandleNewFilePath(fileTraverser.GetNextFile());
-        }
-
-        public void Previous()
-        {
-            HandleNewFilePath(fileTraverser.GetPreviousFile());
-        }
-
-        private void HandleNewFilePath(string filePath)
-        {
-            if (filePath == null) return;
-
-            var wasOldImageNull = CurrentImage == null;
-            CurrentImage = new BitmapImage(new Uri(filePath));
-
-            if (!wasOldImageNull) return;
-
-            UpdateVisibilities();
-            IsImageSelectedChanged(this, IsImageSelected);
-        }
-
-        private void UpdateVisibilities()
-        {
-            var selectVisibility = BoolToVisibilityConverter.ToVisibleOrCollapsed(!IsImageSelected);
-            var otherControlsVisibility = BoolToVisibilityConverter.ToVisibleOrCollapsed(IsImageSelected);
-
-            SelectFileVisibility = selectVisibility;
-
-            PreviousVisibility = otherControlsVisibility;
-            NextVisibility = otherControlsVisibility;
-            CurrentImageVisibility = otherControlsVisibility;
-        }
     }
 }
