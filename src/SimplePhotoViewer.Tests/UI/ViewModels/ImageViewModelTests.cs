9fad2bf134815d5e2b31c83c04e28788824181f2
Joshua Egan <josh.egan145@gmail.com>
2014-08-20 19:38:05 -0600

    Improved version of the complete, functional app.

--- src/SimplePhotoViewer.Tests/UI/ViewModels/ImageViewModelTests.cs
+++ src/SimplePhotoViewer.Tests/UI/ViewModels/ImageViewModelTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
 using NUnit.Framework;
 using Rhino.Mocks;
 using SimplePhotoViewer.IO;
@@ -7,188 +9,234 @@ using SimplePhotoViewer.UI.ViewModels;
 
 namespace SimplePhotoViewer.Tests.UI.ViewModels
 {
-//    [TestFixture]
-//    public class ImageViewModelTests
-//    {
-//        private IFileTraverser fileTraverser;
-//        private ImageViewModel imageViewModel;
-//
-//        [SetUp]
-//        public void Setup()
-//        {
-//            fileTraverser = MockRepository.GenerateStub<IFileTraverser>();
-//            fileTraverser.Stub(f => f.SelectFile(Constants.SupportedImageExtensions)).Return(TestFile.KoalaFilePath)
-//                .Repeat.Once();
-//            fileTraverser.Stub(f => f.SelectFile(Constants.SupportedImageExtensions)).Return(TestFile.TulipsFilePath);
-//            fileTraverser.Stub(f => f.GetPreviousFile()).Return(TestFile.JellyfishFilePath);
-//            fileTraverser.Stub(f => f.GetNextFile()).Return(TestFile.LighthouseFilePath);
-//
-//            imageViewModel = new ImageViewModel(fileTraverser);
-//        }
-//
-//        [Test]
-//        public void _00_visibilities_are_initialized_correctly()
-//        {
-//            const bool isImageSelected = false;
-//            AssertThatVisibilitesAreCorrect(isImageSelected);
-//        }
-//
-//        [Test]
-//        public void _01_select_file_calls_file_traverser()
-//        {
-//            imageViewModel.SelectFile();
-//
-//            fileTraverser.AssertWasCalled(f => f.SelectFile(Constants.SupportedImageExtensions));
-//        }
-//
-//        [Test]
-//        public void _02_select_file_assigns_current_image_if_file_is_returned()
-//        {
-//            Assert.IsNull(imageViewModel.CurrentImage);
-//
-//            imageViewModel.SelectFile();
-//
-//            Assert.IsNotNull(imageViewModel.CurrentImage);
-//        }
-//
-//        [Test]
-//        public void _03_select_file_returns_if_null_is_returned()
-//        {
-//            fileTraverser.BackToRecord();
-//            fileTraverser.Replay();
-//
-//            Assert.DoesNotThrow(() => imageViewModel.SelectFile());
-//        }
-//
-//        [Test]
-//        public void _04_select_file_updates_visibilities()
-//        {
-//            imageViewModel.SelectFile();
-//
-//            const bool isImageSelected = true;
-//            AssertThatVisibilitesAreCorrect(isImageSelected);
-//        }
-//
-//        [Test]
-//        public void _05_next_calls_file_traverser()
-//        {
-//            imageViewModel.Next();
-//
-//            fileTraverser.AssertWasCalled(f => f.GetNextFile());
-//        }
-//
-//        [Test]
-//        public void _06_next_assigns_current_image_if_file_is_returned()
-//        {
-//            imageViewModel.SelectFile();
-//            var img = imageViewModel.CurrentImage;
-//
-//            imageViewModel.Next();
-//
-//            Assert.AreNotEqual(img, imageViewModel.CurrentImage);
-//        }
-//
-//        [Test]
-//        public void _07_next_returns_if_null_is_returned()
-//        {
-//            fileTraverser.BackToRecord();
-//            fileTraverser.Replay();
-//
-//            Assert.DoesNotThrow(() => imageViewModel.Next());
-//        }
-//
-//        [Test]
-//        public void _08_previous_calls_file_traverser()
-//        {
-//            imageViewModel.Previous();
-//
-//            fileTraverser.AssertWasCalled(f => f.GetPreviousFile());
-//        }
-//
-//        [Test]
-//        public void _09_previous_assigns_current_image_if_file_is_returned()
-//        {
-//            imageViewModel.SelectFile();
-//            var img = imageViewModel.CurrentImage;
-//
-//            imageViewModel.Previous();
-//
-//            Assert.AreNotEqual(img, imageViewModel.CurrentImage);
-//        }
-//
-//        [Test]
-//        public void _10_previous_returns_if_null_is_returned()
-//        {
-//            fileTraverser.BackToRecord();
-//            fileTraverser.Replay();
-//
-//            Assert.DoesNotThrow(() => imageViewModel.Previous());
-//        }
-//
-//        [Test]
-//        public void _11_reselect_file_calls_file_traverser()
-//        {
-//            imageViewModel.ReSelectFile();
-//
-//            fileTraverser.AssertWasCalled(f => f.SelectFile(Constants.SupportedImageExtensions));
-//        }
-//
-//        [Test]
-//        public void _12_reselect_file_assigns_current_image_if_file_is_returned()
-//        {
-//            imageViewModel.SelectFile();
-//            var img = imageViewModel.CurrentImage;
-//
-//            imageViewModel.ReSelectFile();
-//
-//            Assert.AreNotEqual(img, imageViewModel.CurrentImage);
-//        }
-//
-//        [Test]
-//        public void _13_reselect_file_returns_if_null_is_returned()
-//        {
-//            fileTraverser.BackToRecord();
-//            fileTraverser.Replay();
-//
-//            Assert.DoesNotThrow(() => imageViewModel.ReSelectFile());
-//        }
-//
-//        [Test]
-//        public void _14_is_image_selected_changed_event_is_raised_when_image_is_selected()
-//        {
-//            var c = 0;
-//            imageViewModel.IsImageSelectedChanged += delegate { c++; };
-//
-//            imageViewModel.SelectFile();
-//
-//            Assert.AreEqual(1, c);
-//        }
-//
-//        [Test]
-//        public void _15_is_image_selected_changed_event_is_raised_only_once()
-//        {
-//            var c = 0;
-//            imageViewModel.IsImageSelectedChanged += delegate { c++; };
-//
-//            imageViewModel.SelectFile();
-//            imageViewModel.Next();
-//            imageViewModel.Next();
-//            imageViewModel.ReSelectFile();
-//            imageViewModel.Previous();
-//
-//            Assert.AreEqual(1, c);
-//        }
-//
-//        private void AssertThatVisibilitesAreCorrect(bool isImageSelected)
-//        {
-//            var selectFileButtonVisibility = isImageSelected ? Visibility.Collapsed : Visibility.Visible;
-//            var otherControlsVisibility = isImageSelected ? Visibility.Visible : Visibility.Collapsed;
-//
-//            Assert.AreEqual(selectFileButtonVisibility, imageViewModel.SelectFileVisibility);
-//            Assert.AreEqual(otherControlsVisibility, imageViewModel.PreviousVisibility);
-//            Assert.AreEqual(otherControlsVisibility, imageViewModel.NextVisibility);
-//            Assert.AreEqual(otherControlsVisibility, imageViewModel.CurrentImageVisibility);
-//        }
-//
-//    }
+    [TestFixture]
+    public class ImageViewModelTests
+    {
+        private IFileTraverser fileTraverser;
+        private ImageViewModel imageViewModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            fileTraverser = MockRepository.GenerateStub<IFileTraverser>();
+            fileTraverser.Stub(f => f.SelectFile(Constants.SupportedImageExtensions)).Return(TestFile.KoalaFilePath);
+            fileTraverser.Stub(f => f.GetPreviousFile()).Return(TestFile.JellyfishFilePath);
+            fileTraverser.Stub(f => f.GetNextFile()).Return(TestFile.LighthouseFilePath);
+
+            imageViewModel = new ImageViewModel(fileTraverser);
+        }
+
+        [Test]
+        public void _00_visibilities_are_initialized_correctly()
+        {
+            const bool isImageSelected = false;
+            AssertThatVisibilitesAreCorrect(isImageSelected);
+        }
+
+        [Test]
+        public void _01_select_file_calls_file_traverser()
+        {
+            imageViewModel.SelectFile();
+
+            fileTraverser.AssertWasCalled(f => f.SelectFile(Constants.SupportedImageExtensions));
+        }
+
+        [Test]
+        public void _02_select_file_assigns_current_image_if_file_is_returned()
+        {
+            Assert.IsNull(imageViewModel.CurrentImage);
+
+            imageViewModel.SelectFile();
+
+            Assert.IsNotNull(imageViewModel.CurrentImage);
+        }
+
+        [Test]
+        public void _03_select_file_returns_if_null_is_returned()
+        {
+            fileTraverser.BackToRecord();
+            fileTraverser.Replay();
+
+            Assert.DoesNotThrow(() => imageViewModel.SelectFile());
+        }
+
+        [Test]
+        public void _04_select_file_updates_visibilities()
+        {
+            imageViewModel.SelectFile();
+
+            const bool isImageSelected = true;
+            AssertThatVisibilitesAreCorrect(isImageSelected);
+        }
+
+        [Test]
+        public void _05_next_calls_file_traverser()
+        {
+            imageViewModel.Next();
+
+            fileTraverser.AssertWasCalled(f => f.GetNextFile());
+        }
+
+        [Test]
+        public void _06_next_assigns_current_image_if_file_is_returned()
+        {
+            imageViewModel.SelectFile();
+            var img = imageViewModel.CurrentImage;
+
+            imageViewModel.Next();
+
+            Assert.AreNotEqual(img, imageViewModel.CurrentImage);
+        }
+
+        [Test]
+        public void _07_next_returns_if_null_is_returned()
+        {
+            fileTraverser.BackToRecord();
+            fileTraverser.Replay();
+
+            Assert.DoesNotThrow(() => imageViewModel.Next());
+        }
+
+        [Test]
+        public void _08_previous_calls_file_traverser()
+        {
+            imageViewModel.Previous();
+
+            fileTraverser.AssertWasCalled(f => f.GetPreviousFile());
+        }
+
+        [Test]
+        public void _09_previous_assigns_current_image_if_file_is_returned()
+        {
+            imageViewModel.SelectFile();
+            var img = imageViewModel.CurrentImage;
+
+            imageViewModel.Previous();
+
+            Assert.AreNotEqual(img, imageViewModel.CurrentImage);
+        }
+
+        [Test]
+        public void _10_previous_returns_if_null_is_returned()
+        {
+            fileTraverser.BackToRecord();
+            fileTraverser.Replay();
+
+            Assert.DoesNotThrow(() => imageViewModel.Previous());
+        }
+
+        [Test]
+        public void _11_reselect_file_calls_file_traverser()
+        {
+            imageViewModel.ReSelectFile();
+
+            fileTraverser.AssertWasCalled(f => f.SelectFile(Constants.SupportedImageExtensions));
+        }
+
+        [Test]
+        public void _12_reselect_file_assigns_current_image_if_file_is_returned()
+        {
+            imageViewModel.SelectFile();
+            var img = imageViewModel.CurrentImage;
+
+            imageViewModel.ReSelectFile();
+
+            Assert.AreNotEqual(img, imageViewModel.CurrentImage);
+        }
+
+        [Test]
+        public void _13_reselect_file_returns_if_null_is_returned()
+        {
+            fileTraverser.BackToRecord();
+            fileTraverser.Replay();
+
+            Assert.DoesNotThrow(() => imageViewModel.ReSelectFile());
+        }
+
+        [Test]
+        public void _14_is_image_selected_changed_event_is_raised_when_image_is_selected()
+        {
+            var c = 0;
+            imageViewModel.IsImageSelectedChanged += delegate { c++; };
+
+            imageViewModel.SelectFile();
+
+            Assert.AreEqual(1, c);
+        }
+
+        [Test]
+        public void _15_is_image_selected_changed_event_is_raised_only_once()
+        {
+            fileTraverser.BackToRecord();
+            fileTraverser.Replay();
+
+            fileTraverser.Stub(f => f.SelectFile(Constants.SupportedImageExtensions)).Return(null).Repeat.Twice();
+            fileTraverser.Stub(f => f.SelectFile(Constants.SupportedImageExtensions)).Return(TestFile.TulipsFilePath);
+
+            var c = 0;
+            imageViewModel.IsImageSelectedChanged += delegate { c++; };
+
+            imageViewModel.SelectFile();
+            imageViewModel.SelectFile();
+            imageViewModel.SelectFile();
+            imageViewModel.Next();
+            imageViewModel.Next();
+            imageViewModel.ReSelectFile();
+            imageViewModel.Previous();
+
+            Assert.AreEqual(1, c);
+        }
+
+        [Test]
+        public void _16_programmatically_assigning_current_image_raises_event()
+        {
+            var c = 0;
+            imageViewModel.IsImageSelectedChanged += delegate { c++; };
+            Assert.IsNull(imageViewModel.CurrentImage);
+
+            imageViewModel.CurrentImage = new BitmapImage(new Uri(TestFile.JellyfishFilePath));
+
+            Assert.AreEqual(1, c);
+        }
+
+        [Test]
+        public void _17_programmatically_assigning_current_image_updates_visibilities()
+        {
+            Assert.IsNull(imageViewModel.CurrentImage);
+
+            imageViewModel.CurrentImage = new BitmapImage(new Uri(TestFile.JellyfishFilePath));
+
+            const bool isImageSelected = true;
+            AssertThatVisibilitesAreCorrect(isImageSelected);
+        }
+
+        [Test]
+        public void _18_is_image_selected_changed_event_is_raised_when_changed_from_null()
+        {
+            var c = 0;
+            imageViewModel.IsImageSelectedChanged += delegate { c++; };
+            Assert.IsNull(imageViewModel.CurrentImage);
+
+            imageViewModel.CurrentImage = null;
+            imageViewModel.CurrentImage = null;
+            imageViewModel.CurrentImage = new BitmapImage(new Uri(TestFile.JellyfishFilePath));
+            imageViewModel.CurrentImage = new BitmapImage(new Uri(TestFile.KoalaFilePath));
+            imageViewModel.CurrentImage = null;
+            imageViewModel.CurrentImage = new BitmapImage(new Uri(TestFile.JellyfishFilePath));
+
+            Assert.AreEqual(3, c);
+        }
+
+        private void AssertThatVisibilitesAreCorrect(bool isImageSelected)
+        {
+            var selectFileButtonVisibility = isImageSelected ? Visibility.Collapsed : Visibility.Visible;
+            var otherControlsVisibility = isImageSelected ? Visibility.Visible : Visibility.Collapsed;
+
+            Assert.AreEqual(selectFileButtonVisibility, imageViewModel.SelectFileVisibility);
+            Assert.AreEqual(otherControlsVisibility, imageViewModel.PreviousVisibility);
+            Assert.AreEqual(otherControlsVisibility, imageViewModel.NextVisibility);
+            Assert.AreEqual(otherControlsVisibility, imageViewModel.CurrentImageVisibility);
+        }
+
+    }
 }
--- src/SimplePhotoViewer/UI/ViewModels/ImageViewModel.cs
+++ src/SimplePhotoViewer/UI/ViewModels/ImageViewModel.cs
@@ -15,12 +15,122 @@ namespace SimplePhotoViewer.UI.ViewModels
 
     public class ImageViewModel : PropertyChangedBase, IImageViewModel
     {
+        private readonly IFileTraverser fileTraverser;
 
-        public event EventHandler<bool> IsImageSelectedChanged;
+        private BitmapSource currentImage;
+        private Visibility currentImageVisibility;
+        private Visibility nextVisibility;
+        private Visibility previousVisibility;
+        private Visibility selectFileVisibility;
+
+        [Inject]
+        public ImageViewModel(IFileTraverser fileTraverser)
+        {
+            this.fileTraverser = fileTraverser;
+
+            UpdateVisibilites();
+        }
+
+        public Visibility SelectFileVisibility
+        {
+            get { return selectFileVisibility; }
+            set
+            {
+                selectFileVisibility = value;
+                NotifyOfPropertyChange(() => SelectFileVisibility);
+            }
+        }
+
+        public Visibility PreviousVisibility
+        {
+            get { return previousVisibility; }
+            set
+            {
+                previousVisibility = value;
+                NotifyOfPropertyChange(() => PreviousVisibility);
+            }
+        }
+
+        public Visibility NextVisibility
+        {
+            get { return nextVisibility; }
+            set
+            {
+                nextVisibility = value;
+                NotifyOfPropertyChange(() => NextVisibility);
+            }
+        }
+
+        public Visibility CurrentImageVisibility
+        {
+            get { return currentImageVisibility; }
+            set
+            {
+                currentImageVisibility = value;
+                NotifyOfPropertyChange(() => CurrentImageVisibility);
+            }
+        }
+
+        public BitmapSource CurrentImage
+        {
+            get { return currentImage; }
+            set
+            {
+                var isOldImageNull = currentImage == null;
+                var isNewImageNull = value == null;
+
+                currentImage = value;
+                NotifyOfPropertyChange(() => CurrentImage);
+
+                if (isOldImageNull == isNewImageNull) return;
+
+                UpdateVisibilites();
+                IsImageSelectedChanged(this, IsImageSelected);
+            }
+        }
+
+        private bool IsImageSelected
+        {
+            get { return CurrentImage != null; }
+        }
+
+        public event EventHandler<bool> IsImageSelectedChanged = delegate { };
 
         public void ReSelectFile()
         {
-            throw new NotImplementedException();
+            UpdateCurrentImage(fileTraverser.SelectFile(Constants.SupportedImageExtensions));
+        }
+
+        public void SelectFile()
+        {
+            UpdateCurrentImage(fileTraverser.SelectFile(Constants.SupportedImageExtensions));
+        }
+
+        public void Next()
+        {
+            UpdateCurrentImage(fileTraverser.GetNextFile());
+        }
+
+        public void Previous()
+        {
+            UpdateCurrentImage(fileTraverser.GetPreviousFile());
+        }
+
+        private void UpdateCurrentImage(string filePath)
+        {
+            if (filePath == null) return;
+            CurrentImage = new BitmapImage(new Uri(filePath));
+        }
+
+        private void UpdateVisibilites()
+        {
+            var selectVisibility = BoolToVisibilityConverter.ToVisibleOrCollapsed(!IsImageSelected);
+            var otherVisibility = BoolToVisibilityConverter.ToVisibleOrCollapsed(IsImageSelected);
+
+            SelectFileVisibility = selectVisibility;
+            PreviousVisibility = otherVisibility;
+            NextVisibility = otherVisibility;
+            CurrentImageVisibility = otherVisibility;
         }
 
     }
